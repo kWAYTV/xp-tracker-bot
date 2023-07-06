b01d706296caeedd2882b27e11d516f23e797621
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-07-06 09:08:24 +0200

    💜Finally fixed it

--- src/controllers/steam.controller.js
+++ src/controllers/steam.controller.js
@@ -6,21 +6,28 @@ require('dotenv').config();
 let user = new SteamUser();
 let csgo = new GlobalOffensive(user);
 
-console.log("Logging into Steam");
-user.logOn({
-    accountName: process.env.STEAM_ACCOUNT_NAME,
-    password: process.env.STEAM_PASSWORD,
-});
-
-user.on('loggedOn', () => {
-    console.log(`Logged into Steam (${user.steamID})`);
+function start_gc() {
+    csgo.on("connectedToGC", () => {
+        console.log("GC started");
+    });
     console.log("Starting CS:GO");
     user.gamesPlayed([730]);
-});
+}
 
-csgo.on("connectedToGC", () => {
-    console.log("GC started");
-});
+module.exports.startCSGO = function() {
+    return new Promise(resolve => {
+        console.log("Logging into Steam");
+        user.logOn({
+            accountName: process.env.STEAM_ACCOUNT_NAME,
+            password: process.env.STEAM_PASSWORD,
+        });
+        user.on('loggedOn', () => {
+            console.log(`Logged into Steam (${user.steamID})`);
+            start_gc();
+            resolve();
+        });
+    });
+}
 
 csgo.on("disconnectedFromGC", () => {
     console.log("GC stopped");
@@ -43,10 +50,10 @@ module.exports.request_player_medals = function (steamID) {
                 return reject({ success: false, error: err });
             }
 
-            let medal_ids = data["medals"] && data["medals"]["display_items_defidx"];
+            let medal_ids = data && data["medals"]["display_items_defidx"];
             if (medal_ids) {
                 // Map the ids to names and PNG file names
-                let medals = medals_controller.get_medal_ids(medal_ids);
+                let medals = medals_controller.get_medals(medal_ids);
                 resolve({ success: true, steamid64: steamID, medals: medals });
             }
             else {
