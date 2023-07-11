14e969306c62471326b7b15b29632b4b8bb1c584
johannes <johannes.werner@dynatech.de>
2023-07-11 15:31:10 +0200

    ✨ Changed JSON Response to render index file

--- api/src/routes/index.route.ts
+++ api/src/routes/index.route.ts
@@ -1,10 +1,8 @@
 import {Application, Request, Response} from 'express';
+import renderPage from '../helpers/view.helper';
 
 export default function (serverInstance: Application) {
   serverInstance.get('/', async (_: Request, res: Response) => {
-    return res.status(200).json({
-      success: true,
-      message: 'Welcome to kwayservices - CSGO Tracking API',
-    });
+    return renderPage(res, 'index', {title: 'kWAY - CSGO Tracking API'});
   });
 }
