8839fa3c4497965dccfee8e7e4f940dadc894afe
johannes <johannes.werner@dynatech.de>
2023-07-11 15:57:51 +0200

    🚚 Renamd static assets path

--- api/src/routes/static.route.ts
+++ api/src/routes/static.route.ts
@@ -2,5 +2,8 @@ import express, {Application} from 'express';
 import {join} from 'node:path';
 
 export default function (serverInstance: Application) {
-  serverInstance.use('/public', express.static(join(__dirname, '../public/')));
+  serverInstance.use(
+    '/public',
+    express.static(join(__dirname, '../www/public/'))
+  );
 }
