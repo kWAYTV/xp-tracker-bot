3badca8c17324f0a87b7969e53388b9c6fcfaad4
darby <violettruhe1@gmail.com>
2023-07-13 00:34:43 +0200

    ✨ Allow loading scripts from inside the body tag

--- api/src/middlewares/helmet.middleware.ts
+++ api/src/middlewares/helmet.middleware.ts
@@ -2,5 +2,15 @@ import {Application} from 'express';
 import helmet from 'helmet';
 
 export default function (serverInstance: Application) {
-  serverInstance.use(helmet());
+  serverInstance.use(
+    helmet({
+      // Enable loading scripts from inside the <body>-Tag
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+        },
+      },
+    })
+  );
 }
